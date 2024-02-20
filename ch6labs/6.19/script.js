a0226cd6d32c91dbd5c91ff42ef76f0d2d4b4be0
tnttruong <thomasnttruong@gmail.com>
2024-02-20 01:37:52 -0600

    all of ch6 labs

--- ch6labs/6.19/script.js
+++ ch6labs/6.19/script.js
@@ -1,4 +1,44 @@
 // Put your solution here
+function compareNumbers(a, b){
+    //console.log(a-b);
+    return a-b;
+}
+
 function divideArray(nums){
+    let even = [];
+    let odd = [];
+
+    //split array into even/odd
+    for(i=0; i<nums.length; i++){
+        if(nums[i] % 2 ==0){
+            even.push(nums[i]);
+        }
+
+        else{
+            odd.push(nums[i]);
+        }
+    }
+    even.sort(compareNumbers);
+    odd.sort(compareNumbers);
     
+    //return arrays
+    console.log("Even numbers:");
+    if(even.length == 0){
+        console.log("None");
+    }
+    else{
+        for(i=0; i<even.length; i++){
+            console.log(even[i]);
+        }
+    }
+
+    console.log("Odd numbers:");
+    if(odd.length == 0){
+        console.log("None");
+    }
+    else{
+        for(i=0; i<odd.length; i++){
+            console.log(odd[i]);
+        }
+    }
 }
---
+++ ch6labs/6.21/script.js
@@ -0,0 +1,29 @@
+// Your solution goes here 
+function playGuessingGame(numToGuess, totalGuesses=10){
+    let guess = 0;
+    let guessCount = 0;
+
+    guess = prompt("Enter a number between 1 and 100.");
+
+    while(guessCount < totalGuesses){
+        if(isNaN(guess) == true || guess == ""){
+            guess = prompt("Please enter a number.");
+        }
+        else if(parseInt(guess) < numToGuess){
+            guessCount++;
+            guess = prompt(guess + ' is too small. Guess a larger number.');
+        }
+        else if(parseInt(guess) > numToGuess){
+            guessCount++;
+            guess = prompt(guess + ' is too large. Guess a smaller number.');
+        }
+        else if(guess == numToGuess){
+            return guessCount + 1;
+        }
+        else if(guessCount == totalGuesses){
+            alert("ran out of guesses");
+        }
+    }
+
+    return 0;
+}
