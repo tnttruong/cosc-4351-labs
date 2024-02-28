829ea6153fd0db1dc834c9542186d4fde14c6104
tnttruong <thomasnttruong@gmail.com>
2024-02-28 02:01:53 -0600

    added ch7

---
+++ ch7labs/7.14/index.js
@@ -0,0 +1,17 @@
+function parseScores(scoresString) {
+    // Your code here
+}
+
+function buildDistributionArray(scoresArray) {
+    // Your code here
+}
+
+function setTableContent(userInput) {
+    // Your code here
+}
+
+function bodyLoaded() {
+    // The argument passed to writeTableContent can be changed for 
+    // testing purposes
+    setTableContent("45 78 98 83 86 99 90 59");
+}
---
+++ ch7labs/7.15/index.js
@@ -0,0 +1,14 @@
+function parseScores(scoresString) {
+   // TODO: Compete the function
+}
+
+function buildDistributionArray(scoresArray) {
+   // TODO: Compete the function
+}
+
+function setTableContent(userInput) {
+   // TODO: Compete the function
+}
+
+// The argument can be changed for testing purposes
+setTableContent("45 78 98 83 86 99 90 59");
